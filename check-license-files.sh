2c7ff9a8c1638d6206040a469b57c35ece940a5c
M.P. Korstanje <rien.korstanje@gmail.com>
2024-07-17 00:02:25 +0200

    Check license files in language subdirectories

--- check-license-files.sh
+++ check-license-files.sh
@@ -16,22 +16,10 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-ORGANISATION=cucumber
-TOPIC=tidelift
-#use polyglot-release for a larger selection
-#TOPIC=polyglot-release
-REPOSITORIES=$(gh repo list "$ORGANISATION" --topic "$TOPIC" --no-archived --limit 999)
-
 EXPECTED=$(grep -v "Copyright (c)" < cucumber/polyglot-developer/LICENSE)
 
-echo "$REPOSITORIES" | while read -r REPOSITORY _; do
-  LICENSE="$REPOSITORY/LICENSE"
-
-  if [ ! -f "$LICENSE" ]; then
-      echo -e "${RED}X${NC} $LICENSE - not found"
-      continue
-  fi
-
+function checkLicense() {
+  LICENSE=$1
   ACTUAL=$(grep -v "Copyright (c)" < "$LICENSE")
   if diff <(echo "$EXPECTED") <(echo "$ACTUAL") &>/dev/null ; then
     echo -e "${GREEN}V${NC} $LICENSE"
@@ -41,4 +29,34 @@ echo "$REPOSITORIES" | while read -r REPOSITORY _; do
     echo
     echo
   fi
+}
+
+ORGANISATION=cucumber
+TOPIC=tidelift
+#use polyglot-release for a larger selection
+#TOPIC=polyglot-release
+REPOSITORIES=$(gh repo list "$ORGANISATION" --topic "$TOPIC" --no-archived --limit 999)
+
+echo "$REPOSITORIES" | while read -r REPOSITORY _; do
+  LICENSE="$REPOSITORY/LICENSE"
+
+  if [ ! -f "$LICENSE" ]; then
+      echo -e "${RED}X${NC} $LICENSE - not found"
+      continue
+  fi
+  checkLicense "$LICENSE"
+
+  for LANGUAGE in c cpp dart dotnet elixir go java javascript objective-c perl php python ruby; do
+    if [ ! -d "$REPOSITORY/$LANGUAGE" ]; then
+      continue
+    fi
+
+    LICENSE="$REPOSITORY/$LANGUAGE/LICENSE"
+    if [ ! -f "$LICENSE" ]; then
+        echo -e "${RED}X${NC} $LICENSE - not found"
+        continue
+    fi
+    checkLicense "$LICENSE"
+  done
+
 done
